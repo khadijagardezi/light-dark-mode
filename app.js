6566f65e1487b6f1608f36561f7c74e8b25d4788
Khadija Batool Gardezi <41299669+khadijagardezi@users.noreply.github.com>
2020-07-24 20:36:52 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,11 @@
+function light() {
+   document.body.style.backgroundColor = "#1b262c";
+   document.body.style.color = "white";
+   document.querySelector('.black').style.color = "#ff6363";
+   document.querySelector('.blue').style.color = "yellow";
+   document.querySelector('.h3').style.color = "yellow";
+   document.getElementById('h3').style.color = "yellow";
+}
+// function light(){
+//    window.location = "index.html";
+//  }
